d1e1a3c2230d94050fcb9543f66eb4da48857e36
Chip Cressman <chip@skilljar.com>
2017-06-30 10:21:02 -0700

    update background image

--- Gruntfile.js
+++ Gruntfile.js
@@ -106,7 +106,7 @@ module.exports = function(grunt) {
         files: [{
           expand: true,
           cwd: 'src/',
-          src: ['**/*.{png,jpg,gif}'],
+          src: ['**/*.{png,jpg,jpeg,gif}'],
           dest: 'dist/'
         }]
       }
